c9e4e7bd4ff0d655d03c6392f88813367eda6f99
AxelBresa <bresanovichaxel43@gmail.com>
2024-03-30 23:33:13 -0300

    se creo varias carpetas para trabajar con el dom en cada una de ellas

---
+++ toping-pizza/script.js
@@ -0,0 +1,45 @@
+//crear un elemento
+
+
+// //agregar stilos//
+// // const primerToping=document.querySelector(".topping")
+// // primerToping.style.backgroundColor="orange"
+// // primerToping.style.color="green"
+
+// //agregar clases//
+// const primerToping=document.querySelector(".topping")
+// primerToping.classList.add("texto-verde")
+
+// console.log(primerToping.classList)
+
+// //eliminar clase//
+// // primerToping.classList.remove("texto-verde")
+
+// //verificar si una clase existe//
+// console.log(primerToping.classList.contains("texto-verde"))
+
+//buscar por id//
+// const contenedor=document.getElementById("contenedor")
+// console.log(contenedor)
+
+//buscar por clase//
+// const toppings=document.getElementsByClassName("topping")
+// console.log(toppings)
+
+//buscar por etiqueta//
+// const misToppings=document.getElementsByTagName("li")
+// console.log(misToppings)
+
+//buscar por elementos o selectores//
+//solo uno (el primero)
+// const topingRojo=document.querySelector(".topping.rojo")
+// console.log(topingRojo)
+
+// //todos los elementos
+// const topingRojo=document.querySelectorAll(".topping.rojo")
+// console.log(topingRojo)
+
+//operador con un not
+// const topingRojo=document.querySelector("ul li:not(.rojo)")
+// console.log(topingRojo)
+
